57f72faf644f2c1c85752c80d6feb37dcfc7670d
GabrielaAmbos <gabrielaambos15@gmail.com>
2020-04-10 18:40:00 -0300

    Adicionando os exercicios
    Exercicios 2, 3, 4, 11

--- src/Exercicios.java
+++ src/Exercicios.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Exercicios {
 
 
-
     private boolean verificaSeONumeroEstaNoArray(int [] array, int n) {
         int posicao = array.length;
         if(array[posicao] == n) {
@@ -37,6 +36,87 @@ public class Exercicios {
         } else {
             System.out.println("Ah, que pena. Ce errou sua pamonha");
         }
+        in.close();
     }
-
+    
+    
+    
+    private boolean verificaNumeroPrimo(int n, int auxiliar) {
+    	if(n == 0 && n == 1) {
+    		return false;
+    	} else {
+    		if(auxiliar == 1) {
+    			return true;
+    		} else {
+    			if(n % auxiliar == 0) {
+    				return false;
+    			} else {
+    				return verificaNumeroPrimo(n, (auxiliar - 1));
+    			}
+    		}
+    	}
+    }
+    
+    public void exercicio2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o numero que voce quer testar: ");
+        int numero = in.nextInt();
+        if(verificaNumeroPrimo(numero, (numero-1))){
+        	System.out.println("Opaaaa, e primo em!");
+        } else {
+        	System.out.println("Oxe, e composto!");
+        }
+        in.close();
+    }
+    
+    
+   // private String inverteString(String mensagem, int posicao) {
+    //	int tamanho = mensagem.length();
+    //	int aux = tamanho-1;
+    //	to
+    //}
+    
+    //private String inverteString(String mensagem, int maior, int menor) {
+    	
+    //}
+    
+    
+    
+    private boolean somaNumeroPerfeito(int numero, int anterior, int soma) {
+    	if(anterior == 1) {
+    		soma++;
+    		if(soma == numero) {
+    			return true;
+    		} else {
+    			return false;
+    		}
+    	} else {
+    		if((numero % anterior) == 0) {
+    			soma += anterior;
+    			return somaNumeroPerfeito(numero, (anterior-1), soma);
+    		} else {
+    			return somaNumeroPerfeito(numero, (anterior-1), soma);
+    		}
+    	}
+    }
+    
+    public void exercicio11() {
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Digite um numero: ");
+    	int numero = in.nextInt();
+    	if(somaNumeroPerfeito(numero, (numero-1), 0)){
+    		System.out.println("Que perfeicao!!!!!");
+    	} else {
+    		System.out.println("Que pena, nao e perfeito, e boy lixo :`c");
+    	}
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    }
+    	Scanner in = new Scanner(System.in);
+    	Exercicios chama = new Exercicios();
+    	chama.exercicio11();
+    }	
 }
