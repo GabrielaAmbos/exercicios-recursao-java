d95efc7577b96d4d418be1818def4444b4442db0
GabrielaAmbos <gabrielaambos15@gmail.com>
2020-04-10 19:52:15 -0300

    Adicionando os exercicios
    Exercicios 6, 9 e 10

--- src/Exercicios.java
+++ src/Exercicios.java
@@ -39,8 +39,6 @@ public class Exercicios {
         in.close();
     }
     
-    
-    
     private boolean verificaNumeroPrimo(int n, int auxiliar) {
     	if(n == 0 && n == 1) {
     		return false;
@@ -82,6 +80,62 @@ public class Exercicios {
     
     
     
+    
+    private double somaNumerosNPorUm(int n) {
+    	if(n == 1) {
+    		return n;
+    	} else {
+    		return (double)1/n + somaNumerosNPorUm(n-1); 
+    	}
+    }
+    
+    public void exercicio6() {
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Digite um numero: ");
+    	int numero = in.nextInt();
+    	System.out.println(somaNumerosNPorUm(numero));
+    	in.close();
+    }
+    
+    private int verificaSeDEstaDentroDeN(int d, int n) {
+    	if(n < 1) {
+    		return 0;
+    	} else {
+    		int aux = n % 10;
+    		if(d == aux) {
+    			return 1 + verificaSeDEstaDentroDeN(d, n/10);
+    		} else {
+    			return verificaSeDEstaDentroDeN(d, n/10);
+    		}
+    	}
+    }
+    
+    public void exercicio9() {
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Digite um numero: ");
+    	int numero = in.nextInt();
+    	System.out.println(verificaSeDEstaDentroDeN(1, numero));
+    	in.close();
+    }
+    
+     private double mediaDosDigitos(int n, int quantDigitos) {
+    	if(n < 1) {
+    		return n;
+    	} else {
+    		double aux = n % 10;
+    		return aux/quantDigitos + mediaDosDigitos(n/10, quantDigitos);
+    	}
+    }
+    
+    public void exercicio10() {
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Digite um numero: ");
+    	int numero = in.nextInt();
+    	int tamanho = Integer.toString(numero).length();
+    	System.out.println(mediaDosDigitos(numero, tamanho));
+    	in.close();
+    }
+    
     private boolean somaNumeroPerfeito(int numero, int anterior, int soma) {
     	if(anterior == 1) {
     		soma++;
@@ -109,14 +163,7 @@ public class Exercicios {
     	} else {
     		System.out.println("Que pena, nao e perfeito, e boy lixo :`c");
     	}
+    	in.close();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    	Scanner in = new Scanner(System.in);
+    	
     	Exercicios chama = new Exercicios();
-    	chama.exercicio11();
+    	//chama.exercicio11();
+    	//chama.exercicio6();
+    	//chama.exercicio10();
+    	//
+    	chama.exercicio9();
+    	
     }	
 }
